b614307cbd87813b9cb14dce837aa4ffb64024bd
Diego Lázaro <111025421+diegolazarocs@users.noreply.github.com>
2024-06-23 00:03:15 -0300

    Update main.js

--- Input Files Drop/main.js
+++ Input Files Drop/main.js
@@ -10,3 +10,27 @@ label.addEventListener("dragenter", onEnter);
 label.addEventListener("drop", onLeave);
 label.addEventListener("dragend", onLeave);
 label.addEventListener("dragleave", onLeave);
+
+const input = document.querySelector("input");
+const dropZone = document.querySelector("#drop-zone");
+
+input.addEventListener("change", (event) => {
+  if (input.files.length > 0) {
+    const type = input.files[0].type;
+    const formarts = ["image/jpeg", "image/png", "image/jpg"];
+    if (!formarts.includes(type)) {
+      alert("Esse formato não é permitido!");
+      return;
+    }
+    
+    if (document.querySelector("#cover")) {
+      dropZone.removeChild(document.querySelector("#cover"));
+    }
+
+    const img = document.createElement("img");
+    img.id = "cover";
+    img.src = URL.createObjectURL(input.files[0]);
+
+    dropZone.appendChild(img);
+  }
+});
